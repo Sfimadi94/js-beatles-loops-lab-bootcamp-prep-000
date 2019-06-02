14cde9628c6805a73f57b90f30fd63186e8fbe05
charming-reference-7654 <sydeffect1994@gmail.com>
2019-06-02 06:15:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var arrays = [];
   for (var i = 0; i < musicians.length; i++) {
-    arrays.push(musicians[i] + "plays" + instruments[i]);
+    arrays.push(musicians[i] + " plays " + instruments[i]);
   }
   return arrays
 }
