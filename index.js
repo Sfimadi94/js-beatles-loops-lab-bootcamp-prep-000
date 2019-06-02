574add8faedab5e2827a9ddd42b5a258685bea3a
charming-reference-7654 <sydeffect1994@gmail.com>
2019-06-02 06:11:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var empty = {};
-  for (var i = 0; i < musicians.length; i++);
+  for (var i = 0; i < musicians.length; i++) {
+    
+  }
 }
